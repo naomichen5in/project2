d11529b1aca275197d7fa01ecf4ca1e45026e51d
Naomi <naomichen.5in@gmail.com>
2026-04-23 15:52:01 +0800

    Calculator stage 1

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,11 +1,31 @@
-let number=0;
-let addonebutton=document.getElementById("counter-button");
-let counter=document.getElementById("counter");
+//Make Variables
+let number = 0;
+let addonebutton = document.getElementById("counter-button");
+let counter = document.getElementById("counter");
+let delbutton = document.getElementById("del-button");
+let minusbutton = document.getElementById("minus-button");
 
- function add1(){
-       number= number+1;
-   counter.textContent=number;}
+//Define Functions
+function add1() {
+    number = number + 1;
+    counter.textContent = number;
+}
 
+
+function minus() {
+    number = number - 1;
+    counter.textContent = number;
+}
+
+
+function del() {
+    number = 0;
+    counter.textContent = number;
+}
+
+
+//Define Buttons Event Listeners
 addonebutton.addEventListener("click", add1);
-
+minusbutton.addEventListener("click", minus);
+delbutton.addEventListener("click", del);
 
